cf770ca1746ad80827ec6955c27e67bef793c769
Vladimir Vince <vladimirvince@gmail.com>
2017-11-25 01:55:07 -0500

    gitignore

--- twitter-bot/config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-module.exports = {
-  consumer_key: 'ygScUy9lWlBQt8bi4Kf7vSUT5',
-  consumer_secret: 'dMfT0Eaycyn5uXBlevSaErtFx0KxJ0f42nxa66mCoq5e8LGRD3',
-  access_token_key: '934292336170946560-Gsv0E9rs7esfqVknUlJf22CxipIJlM1',
-  access_token_secret: 'KkzIlH0xeAXx7Mitxq7KVmZwQgfJKNXwu9ljoeBnYGWFL'
-}
