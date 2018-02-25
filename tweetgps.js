57fb56151eb733ef6d108cb084e66d0fe987cefa
mejs <vladimirvince@gmail.com>
2018-02-24 21:34:55 -0500

    integrated tweet+gps

--- tweetgps.js
+++ tweetgps.js
@@ -1,27 +1,16 @@
-var fs = require('fs');
+var SerialPort = require('serialport');
+var port = new SerialPort.SerialPort('/dev/serial0', { 
+  baudrate: 9600,
+  parser: SerialPort.parsers.readline('\r\n')
+});
+ 
 var GPS = require('gps');
 var gps = new GPS;
-var twitter = require('twitter');
-var config = require('./config.js');
-var T = new twitter(config);
-var curl = require('curlrequest');
-photo = fs.readFileSync('/tmp/tweet.jpg', { encoding: 'base64' });
-const file = '/dev/serial0';
-const SerialPort = require('serialport');
-const parsers = SerialPort.parsers;
-const parser = new parsers.Readline({ delimiter: '\r\n' });
-const port = new SerialPort(file, { baudRate: 9600 });
-port.pipe(parser);
-
-//gps grabs location
-
-gps.on('data', function() {
-	var location = `${gps.state.lat},${gps.state.lon}`
-	console.log(location);
-  });
-
-  parser.on('data', function(data) {
-    gps.update(data);
+ 
+gps.on('data', function(data) {
+  console.log(data, gps.state);
 });
-
-
+ 
+port.on('data', function(data) {
+  gps.update(data);
+});
