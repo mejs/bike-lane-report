7ff2c8ace0b1838a317ffe444e5b9049fb653908
Vladimir Vince <vladimirvince@gmail.com>
2018-02-25 00:01:55 -0500

    Delete tweetgps.js

--- tweetgps.js
+++
@@ -1,8 +0,0 @@
-var SerialPort = require('serialport');
-var port = new SerialPort.SerialPort('/dev/serial0', { 
-  baudrate: 9600,
-  parser: SerialPort.parsers.readline('\r\n')
-});
-
-var GPS = require('gps');
-var gps = new GPS;
