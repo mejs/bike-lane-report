7cc319ef885fac49bf9c26fc8ad4d69b660167ef
Vladimir <vladimirvince@gmail.com>
2018-02-25 00:41:11 -0500

    config removed from repository

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-module.exports = {
-  consumer_key: '7rEOQfXYX46JfdU3nZOL0MEQz',
-  consumer_secret: 'n0MS2c0Cymee6hlvPDPg4usGIRxSm0UPF056Zo2ODAXpLn2IRE',
-  access_token_key: '934292336170946560-Gsv0E9rs7esfqVknUlJf22CxipIJlM1',
-  access_token_secret: 'KkzIlH0xeAXx7Mitxq7KVmZwQgfJKNXwu9ljoeBnYGWFL'
-}
